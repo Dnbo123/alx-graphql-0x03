b44c13ea4e682d0b9e70be21116b06aa4a9b195b
Don Bosco <don965877@gmail.com>
2025-01-24 07:15:14 +0300

    Query the graphql endpoint

--- alx-rick-and-morty-app/graphql/queries.ts
+++ alx-rick-and-morty-app/graphql/queries.ts
@@ -1,11 +1,11 @@
 import {gql} from"@apollo/client"
 
 export const GET_EPISODES = gql`
-  query getEpisodes=($page: Int, $filter: FilterEpisode) {
+  query getEpisodes($page: Int, $filter: FilterEpisode) {
     episodes(page: $page, filter: $filter) {
         info {
             pages
-            nexy
+            next
             prev
             count
         }
